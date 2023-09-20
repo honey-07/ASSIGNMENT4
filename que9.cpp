d3be9abd3197cb513b5de2133195a5a50c6dc12d
HONEY PATEL <123378526+honey-07@users.noreply.github.com>
2023-09-20 11:29:06 +0530

    Update que9.cpp

--- que9.cpp
+++ que9.cpp
@@ -1,1 +1,24 @@
+//Write a program to display the reverse of a given number using template based function.
+
+#include <iostream>
+using namespace std;
+
+template <class t>
+void reverse(t num) {
+  t rev = 0,rem;
+  while (num > 0) {
+    rem = num%10;
+    rev=rev*10+rem;
+    num /= 10;
+  }
+  cout << "Reverse of a number is : " << rev << endl;
+}
+
+int main() {
+  int num;
+  cout << "Enter a number: ";
+  cin >> num;
+  reverse(num);
+  return 0;
+}
 
